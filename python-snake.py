6331999753b5b2c1899810bc2a6e28e00ac4ed3c
Andrew Taylor <mac@andrewt.net>
2016-03-26 15:25:12 +0000

    Python Tetris

--- python-snake.py
+++ python-snake.py
@@ -1,27 +1,15 @@
 from microbit import *
 import random
 
-tetris = Image("00090:00990:90000:99909:90999")
-snake =  Image("55550:50050:50000:50090:55000")
-games = [ tetris, snake ]
-game = 0
+snake = Image("55550:50050:50000:50090:55000")
 score = 0
 
 def play_game():
-    global tetris
-    global snake
-    global games
-    global game
     global score
     while button_a.is_pressed() or button_b.is_pressed():
         pass
     score = 0
-    if games[game] == tetris:
-        play_tetris()
-    elif games[game] == snake:
-        play_snake()
-    else:
-        return
+    play_snake()
     display.scroll(str(score))
     while not button_a.is_pressed() or not button_b.is_pressed():
         pass
@@ -29,26 +17,11 @@ def play_game():
         pass
 
 def choose_game():
-    global games
-    global game
+    display.show(snake)
     while True:
-        display.show(games[game])
-        if button_a.is_pressed():
-            played = False
-            while button_a.is_pressed():
-                if button_b.is_pressed():
-                    play_game()
-                    played = True
-            if not played:
-                game = (game + 1) % len(games)
-        elif button_b.is_pressed():
-            played = False
-            while button_b.is_pressed():
-                if button_a.is_pressed():
-                    play_game()
-                    played = True
-            if not played:
-                game = (game - 1) % len(games)
+        if button_a.is_pressed() and button_b.is_pressed():
+            play_game()
+            display.show(snake)
 
 def is_lit(image, pos):
     return image.get_pixel(pos['x'], pos['y'])
@@ -62,10 +35,12 @@ def unlight_pixel(image, pos):
 a_pressed = False
 b_pressed = False
 on_double = False
+done_this_double = False
 def button_sleep(time, doubles = True, queue = []):
     global a_pressed
     global b_pressed
     global on_double
+    global done_this_double
     while time > 0:
         a_pressed = button_a.is_pressed()
         b_pressed = button_b.is_pressed()
@@ -76,8 +51,13 @@ def button_sleep(time, doubles = True, queue = []):
                 on_double = True
             elif not a_pressed and not b_pressed:
                 on_double = False
-            if on_double and (not button_a.is_pressed() or not button_b.is_pressed()):
+                done_this_double = False
+            if on_double and not done_this_double and (not button_a.is_pressed() or not button_b.is_pressed()):
                 queue.append('ab')
+                done_this_double = True
+        else:
+            on_double = False
+            done_this_double = False
         if not on_double:
             if not button_a.is_pressed() and a_pressed:
                 queue.append('a')
@@ -87,33 +67,6 @@ def button_sleep(time, doubles = True, queue = []):
 
 def random_pos():
     return { 'x': random.randint(0, 4), 'y': random.randint(0, 4) }
-
-def play_tetris():
-    global score
-    im = Image(5, 5)
-    block = None
-    while True:
-        block = random.randint(0, 7)
-        if block == 0:
-            block = [ [ [ 9 ] ] ]
-        elif block <= 2:
-            block = [ [ [ 7, 7 ] ], [ [ 7 ], [ 7 ] ] ]
-        elif block <= 6:
-            block = [ [ [ 5, 5 ], [ 5, 0 ] ], [ [ 5, 5 ], [ 0, 5 ] ], [ [ 0, 5 ], [ 5, 5 ] ], [ [ 5, 0 ], [ 5, 5 ] ] ]
-        else:
-            block = [ [ [ 3, 3 ], [ 3, 3 ] ] ]
-        orientation = random.randint(0, len(block))
-        x = 2
-        y = -1
-        while True:
-            for n in range(0, 30):
-                display.show(im)
-                for press in button_sleep(10, True):
-                    if press == 'a':
-                        orientation = (orientation - 1) % len(block)
-                    elif press == 'b':
-                        orientation = (orientation + 1) % len(block)
-
 
 def play_snake():
     global score
